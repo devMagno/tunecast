5d45c280bb159d041e893d8c65b8abce285f23a2
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-25 14:30:29 -0300

    Adiciona troca de gênero ao clique e atualiza reset de estilos

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ const header = document.querySelector('.header')
 hamburgerButton.addEventListener('click', () => {
   header.classList.toggle('open')
 })
+
+const genderButtons = document.querySelectorAll('.genders__item')
+genderButtons.forEach(el => {
+  el.addEventListener('click', switchGender)
+});
+
+function switchGender(event) {
+  const button = event.currentTarget
+
+  genderButtons.forEach(el => {
+    el.classList.remove('genders__item--purple')
+  });
+
+  button.classList.toggle('genders__item--purple')
+}
