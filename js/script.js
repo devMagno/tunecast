3c7a5d4151ffb13756a89c90c032a72e20897a45
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-25 14:44:47 -0300

    Adiciona troca de formulário ao clique

--- js/script.js
+++ js/script.js
@@ -18,3 +18,21 @@ function switchGender(event) {
 
   button.classList.toggle('genders__item--purple')
 }
+
+
+const buttons = document.querySelectorAll('.buttons__button')
+const forms = document.querySelectorAll('.section__form')
+buttons.forEach((el, index) => {
+  el.addEventListener('click', () => switchForms(index))
+})
+
+function switchForms(index, event) {
+  buttons.forEach(button => {
+    button.classList.remove('buttons__button--purple')
+  })
+  forms.forEach(form => {
+    form.classList.remove('active')
+  })
+  forms[index].classList.add('active')
+  buttons[index].classList.add('buttons__button--purple')
+}
